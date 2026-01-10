d3caa8fb43b672c6e531a2f22052172041b61527
alen4203 <alen4203@gmail.com>
2026-01-10 19:00:48 +0800

    some settings

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // Hardcoded for GitHub Pages deployment
+  basePath: process.env.NODE_ENV === 'production' ? '/Artemisia-Studio' : '',
   images: {
     unoptimized: true,
   },
